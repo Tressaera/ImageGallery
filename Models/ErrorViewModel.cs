ec96ff021ed16423c7eaea829ce3f90f314254c9
Mert Yılmaz <mattshady1067@gmail.com>
2024-03-27 12:04:59 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageGallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
